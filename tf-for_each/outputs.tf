4a088b584563148e56f2db0e3469c8fc94820ef1
anupam <anupamyadav0224@gmail.com>
2023-03-14 00:16:12 +0530

    eip output added.

--- tf-for_each/outputs.tf
+++ tf-for_each/outputs.tf
@@ -4,18 +4,26 @@ output "tst_vpc_subnet" {
   description = "The subnet resource being created"
 }
 
-output "tst_vpc_subnet01_id" {
+output "tst_vpc_subnet_01_id" {
+  description = "The ID of the first subnet being created."
   value       = google_compute_subnetwork.tst_vpc_subnet["fdn-tst-subnet-01"].id
-  description = "The ID of the first subnet."
 }
 
 /*************************************************
   Output using for expression
 *************************************************/
 output "tst_vpc_subnet_ids" {
+  description = "The IDs of the subnets being created."
   value = [
     for instance in google_compute_subnetwork.tst_vpc_subnet :
     instance.id
   ]
-  description = "The tuple of the IDs of the created subnets."
+}
+
+output "tst_eip_ids" {
+  description = "The IDs of the external IPs being reserved."
+  value = [
+    for instance in google_compute_address.tst_eip :
+    instance.id
+  ]
 }

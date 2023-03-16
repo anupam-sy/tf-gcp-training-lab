01fdc6f9a359cd5da2d372dcab90211c6e32ac53
anupam <anupamyadav0224@gmail.com>
2023-03-16 19:39:41 +0530

    google-beta provider config removed.

--- tf-cli-workspaces/providers.tf
+++ tf-cli-workspaces/providers.tf
@@ -4,9 +4,3 @@ provider "google" {
   region  = var.default_region
   zone    = var.default_zone
 }
-
-provider "google-beta" {
-  project = var.project_id
-  region  = var.default_region
-  zone    = var.default_zone
-}

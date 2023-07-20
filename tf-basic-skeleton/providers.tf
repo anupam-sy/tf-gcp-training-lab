50ade016769ab4c48f0be95db28cc3a34584833d
anupam <anupamyadav0224@gmail.com>
2023-07-20 18:26:52 +0530

    bump tf version to 1.5.0

--- tf-basic-skeleton/providers.tf
+++ tf-basic-skeleton/providers.tf
@@ -3,12 +3,12 @@ provider "google" {
   project     = var.project_id
   region      = var.default_region
   zone        = var.default_zone
-  credentials = file("./credentials/tf_mgcp_svc_account.json")
+  # credentials = file("./credentials/tf_mgcp_svc_account.json")
 }
 
 provider "google-beta" {
   project     = var.project_id
   region      = var.default_region
   zone        = var.default_zone
-  credentials = file("./credentials/tf_mgcp_svc_account.json")
+  # credentials = file("./credentials/tf_mgcp_svc_account.json")
 }

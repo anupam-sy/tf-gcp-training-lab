e6ba1f5fd8698900f3202069255b71da9a9bd1e5
anupam <anupamyadav0224@gmail.com>
2023-09-14 22:03:56 +0530

    bump tf version to 1.5.0 and google provider version to 4.82.0

--- tf-variables/versions.tf
+++ tf-variables/versions.tf
@@ -1,15 +1,15 @@
 // Terraform block to configure terraform and provider version
 terraform {
-  required_version = "~> 1.3.6"
+  required_version = "~> 1.5.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.55.0"
+      version = "~> 4.82.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.55.0"
+      version = "~> 4.82.0"
     }
   }
 }

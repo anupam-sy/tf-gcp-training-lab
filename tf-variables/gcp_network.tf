6d565dda02bc924dc1cbc8eab42e20d6c855570a
Anupam <anupaminit@gmail.com>
2024-06-11 12:49:06 +0530

    feat: bumped tf ver to 1.8.0 and updated authentication method

--- tf-variables/gcp_network.tf
+++ tf-variables/gcp_network.tf
@@ -1,9 +1,10 @@
 // Resource block to deploy vpc network
 resource "google_compute_network" "tst_vpc" {
-  project                         = var.project_id
-  name                            = var.vpc_name
-  description                     = var.description
-  routing_mode                    = "GLOBAL"
-  auto_create_subnetworks         = var.auto_create_subnetworks
-  delete_default_routes_on_create = var.delete_default_routes
+  project                                   = var.project_id
+  name                                      = var.vpc_name
+  description                               = var.description
+  routing_mode                              = "GLOBAL" # use variable, if parameter will change between deployments.
+  auto_create_subnetworks                   = var.auto_create_subnetworks
+  delete_default_routes_on_create           = var.delete_default_routes
+  network_firewall_policy_enforcement_order = var.network_firewall_policy_enforcement_order
 }
